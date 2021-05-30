bf6f6f77c744c4c86613135694684a400b53faff
Mehdi Mourjane <mehdi.mourjane@epitech.eu>
2021-05-30 14:10:50 +0200

    [REMAKE] camera implementation with a completly new update camera who can handle gamepadgit add encapsulation/include/Camera.hpp encapsulation/src/Camera.cpp git add encapsulation/include/Camera.hpp encapsulation/src/Camera.cpp git add encapsulation/include/Camera.hpp encapsulation/src/Camera.cpp git add encapsulation/include/Camera.hpp encapsulation/src/Camera.cpp  but not keybord and mouse

--- encapsulation/include/Camera.hpp
+++ encapsulation/include/Camera.hpp
@@ -9,36 +9,65 @@
 #define CAMERA_HPP_
 
 #include "raylib.h"
+#include <utility>
+#include <cmath>
+
 
 namespace rl {
-    class Camera
-    {
+    // class Camera
+    // {
+    //     public:
+    //         Camera();
+    //         Camera(Vector3, CameraMode);
+    //         ~Camera();
+
+    //         // Getters & setters
+    //         void setPosition(Vector3);
+    //         void setTarget(Vector3);
+    //         void setFOV(float);
+    //         void setProjection(CameraProjection);
+    //         void setMode(CameraMode);
+    //         Vector3 getPosition(void) const;
+    //         Vector3 getTarget(void) const;
+    //         float getCameraFOV(void) const;
+    //         void updateCamera();
+
+    //         Camera3D _camera;
+    //     protected:
+    //     private:
+    //         Vector3 _pos;
+    //         Vector3 _target;
+    //         CameraProjection _projection;
+    //         CameraMode _mode;
+    //         float _fov;
+    // };
+    class Camera {
         public:
             Camera();
-            Camera(Vector3, CameraMode);
-            ~Camera();
+            Camera(Vector3 pos, CameraMode mode = CAMERA_FREE);
+            ~Camera() = default;
 
-            // Getters & setters
-            void setPosition(Vector3);
-            void setTarget(Vector3);
-            void setFOV(float);
-            void setProjection(CameraProjection);
-            void setMode(CameraMode);
-            Vector3 getPosition(void) const;
-            Vector3 getTarget(void) const;
-            float getCameraFOV(void) const;
-            void updateCamera();
+            void setPosition(Vector3 value) {_camera.position = value;};
+            void setTarget(Vector3 value) {_camera.target = value;};
+            void setFOV(float value) {_camera.fovy = value;};
+            void setProjection(CameraProjection value) {_camera.projection = value;};
+            void setMode(CameraMode mode) {SetCameraMode(_camera, mode);};
+            void setSpeed(int speed) {_speed = speed;};
+            
+            Vector3 getPosition(void) const {return _camera.position;};
+            Vector3 getTarget(void) const {return _camera.target;};
+            float getCameraFOV(void) const {return _camera.fovy;};
+            Camera3D getCamera() const {return _camera;};
+            int getSpeed() const {return _speed;};
 
-        protected:
+            void updateCamera(std::pair<float, float> gmpAxisLeft = {0.0f, 0.0f}, std::pair<float, float> gmpAxisRight = {0.0f, 0.0f});
+
         private:
-            Camera3D _camera;
-            Vector3 _pos;
-            Vector3 _target;
-            CameraProjection _projection;
-            CameraMode _mode;
-            float _fov;
+            Camera3D _camera = {0};
+            std::pair<float, float> assignpr(float value, float x, float y);
+            std::pair<float, float> getpr(float x, float y);
+            int _speed = 90;
     };
 };
-
 
 #endif /* !CAMERA_HPP_ */
--- encapsulation/src/Camera.cpp
+++ encapsulation/src/Camera.cpp
@@ -6,80 +6,97 @@
 */
 
 #include "Camera.hpp"
+#include <raymath.h>
+#include <iostream>
+
+std::pair<float, float> rl::Camera::getpr(float x, float z)
+{
+    x = std::abs(x);
+    z = std::abs(z);
+    float globalValue = x + z;
+    float xr = x * 100 / globalValue;
+    float zr = xr - 100;
+    return {std::abs(xr), std::abs(zr)};
+}
+
+std::pair<float, float> rl::Camera::assignpr(float value, float x, float y)
+{
+    return {value * (x / 100), value * (y / 100)};
+}
 
 rl::Camera::Camera()
 {
-    Camera3D camera = {0};
-
-    this->_camera = camera;
     this->setPosition( {0.0f, 0.0f, 0.0f} );
     this->setTarget( {0.0f, 0.0f, 0.0f} );
+    _camera.up = {0.0f, 1.0f, 0.0f};
     this->setFOV(45.0);
     this->setProjection(CAMERA_PERSPECTIVE);
     this->setMode(CAMERA_FREE);
 }
 
-rl::Camera::Camera(Vector3 pos, CameraMode mode = CAMERA_FREE)
+rl::Camera::Camera(Vector3 pos, CameraMode mode)
 {
-    Camera3D camera = {0};
-
     this->setPosition(pos);
     this->setTarget( {0.0f, 0.0f, 0.0f} );
+    _camera.up = {0.0f, 1.0f, 0.0f};
     this->setFOV(45.0);
     this->setProjection(CAMERA_PERSPECTIVE);
     this->setMode(mode);
 }
 
-void rl::Camera::setPosition(Vector3 vector)
+Vector2 axToMouse(std::pair<float, float> ax)
 {
-    this->_pos = vector;
-    this->_camera.position = vector;
+    static double x = GetMousePosition().x;
+    static double y = GetMousePosition().y;
+
+    x += ax.first;
+    y += ax.second;
+
+    return (Vector2) {x, y};
 }
 
-void rl::Camera::setTarget(Vector3 vector)
+void rl::Camera::updateCamera(std::pair<float, float> gmpAxisLeft, std::pair<float, float> gmpAxisRight)
 {
-    this->_target = vector;
-    this->_camera.target = vector;
-}
+    static const int mouseSensivity = 600;
+    static const float minimumY = -65.0f;
+    static const float maximumY = 89.0f;
+    static Vector2 previousMousePosition = axToMouse(gmpAxisRight);
+    static Vector2 angle = {0, 0};
 
-void rl::Camera::setFOV(float fov)
-{
-    this->_fov = fov;
-    this->_camera.fovy = fov;
-}
+    //udate pos cam from left axis gamepad
+        if (gmpAxisLeft != std::make_pair(0.0f, 0.0f)) {
+            Vector3 forward = Vector3Subtract(this->getTarget(), this->getPosition());
+            forward.y = 0;
+            forward = Vector3Normalize(forward);
+            Vector3 right = {forward.z * -1.0f, 0, forward.x};
+            Vector3 oldPosition = this->getPosition();
 
-void rl::Camera::setProjection(CameraProjection projection)
-{
-    this->_projection = projection;
-    this->_camera.projection = projection;
-}
+            this->setPosition(Vector3Add(this->getPosition(), Vector3Scale(forward, gmpAxisLeft.second * -1 * 0.0075f))); // 0.0075f is the speed bride
+            this->setPosition(Vector3Add(this->getPosition(), Vector3Scale(right, gmpAxisLeft.first * 0.0075f)));
+        }
+        if (gmpAxisRight != std::make_pair(0.0f, 0.0f)) {
+            Vector2 mousePosition = axToMouse(gmpAxisRight);
+            Vector2 mousePositionDelta = Vector2Subtract(previousMousePosition, mousePosition);
+            previousMousePosition = mousePosition;
+            float turnRotation = gmpAxisRight.first * GetFrameTime() * this->_speed;
+            float tiltRotation = gmpAxisRight.second * GetFrameTime() * this->_speed;
 
-void rl::Camera::setMode(CameraMode mode)
-{
-    this->_mode = mode;
-    SetCameraMode(this->_camera, mode);
-}
+            if (turnRotation != 0)
+                angle.x -= turnRotation * DEG2RAD;
+            else
+                angle.x += mousePositionDelta.x / -mouseSensivity;
+            if (tiltRotation)
+                angle.y += tiltRotation * DEG2RAD;
+            else
+                mousePositionDelta.y / -mouseSensivity;
 
-Vector3 rl::Camera::getPosition(void) const
-{
-    return this->_pos;
-}
-
-Vector3 rl::Camera::getTarget(void) const
-{
-    return this->_target;
-}
-
-float rl::Camera::getCameraFOV(void) const
-{
-    return this->_fov;
-}
-
-void rl::Camera::updateCamera()
-{
-    UpdateCamera(&this->_camera);
-}
-
-rl::Camera::~Camera()
-{
+            if (angle.y < minimumY * DEG2RAD)
+                angle.y = minimumY * DEG2RAD;
+            else if (angle.y > maximumY * DEG2RAD)
+                angle.y = maximumY * DEG2RAD;
+            Vector3 target = Vector3Transform((Vector3) {0, 0, 1}, MatrixRotateXYZ( (Vector3){-angle.y, -angle.x, 0} ));
+            this->setTarget(Vector3Add(this->getPosition(), target));
+        }
+    //update camera vis;
+        // UpdateCamera(&_camera);
 }
