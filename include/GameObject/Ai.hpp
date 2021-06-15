ccceef87bea446f07d0e4d2b8af903c20ff68f9c
Clément DELATTRE <clement.delattre@epitech.eu>
2021-06-15 21:20:14 +0200

    [ADD] Pathfinding algorithm

--- include/GameObject/Ai.hpp
+++ include/GameObject/Ai.hpp
@@ -1,28 +1,32 @@
 #ifndef BOMBERMAN_AI_HPP
 #define BOMBERMAN_AI_HPP
 
+#include <utility>
 #include "EntityObjects.hpp"
 
 enum Target {IN_RANGE, TOO_FAR, ABSENT, UNDEFINED};
 
 class Ai : public EntityObjects {
 public:
-    Ai();
+    Ai(std::vector<std::vector<char>> map);
     ~Ai() = default;
 
-
     Target checkEnemy();
+    RAYLIB::Vector2 getEnemyPosition();
+    double calculateDist(RAYLIB::Vector2 pos);
     void getPriority();
     void moveToEnemy();
-    Vector2 getEnemyPosition();
     void attackEnemy();
     void setRandomTarget();
+    void moveToNextTile();
     std::string serialize();
     void deserialize(std::string str);
 
 protected:
-    Vector2 targetPosition;
+    RAYLIB::Vector2 targetPosition;
     Target targetStatus;
+    std::vector<std::vector<char>> map;
+    std::vector<RAYLIB::Vector2> close;
     };
 
 
--- src/GameObject/Ai.cpp
+++ src/GameObject/Ai.cpp
@@ -1,8 +1,11 @@
 #include "Ai.hpp"
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 
-Ai::Ai()
+Ai::Ai(std::vector<std::vector<char>> map)
 {
+    this->map = map;
     this->targetStatus = UNDEFINED;
 }
 
@@ -11,9 +14,46 @@ Target Ai::checkEnemy()
     return ABSENT;
 }
 
+double Ai::calculateDist(RAYLIB::Vector2 pos)
+{
+    int diff_x = this->_pos.x - pos.x;
+    int diff_y = this->_pos.y - pos.y;
+
+    return sqrt(diff_x * diff_x + diff_y * diff_y);
+}
+
+void Ai::moveToNextTile()
+{
+    std::vector<RAYLIB::Vector2> open;
+    std::vector<float> dist;
+    RAYLIB::Vector2 pos;
+
+    this->close.push_back(this->_pos);
+    for (int y = -1; y < 2; y++) {
+        pos.y = this->_pos.y + y;
+        for (int x = -1; x < 2; x++) {
+            pos.x = this->_pos.x + x;
+            if (this->map[pos.y][pos.x] == ' ' || this-> map[pos.y][pos.x] == 'S')
+                open.push_back(pos);
+        }
+    }
+    for (unsigned int i = 0; i < open.size(); i++)
+        for (unsigned int j = 0; j < this->close.size(); j++)
+            if (open[i].x == this->close[j].x && open[i].y == this->close[j].y) {
+                open.erase(open.begin() + i);
+                i = 0;
+            }
+    for (unsigned int i = 0; i < open.size(); i++)
+        dist.push_back(calculateDist(open[i]));
+    auto min_value = std::min_element(dist.begin(), dist.end());
+    setPos(open[min_value - dist.begin()]);
+    // set direction
+}
+
 void Ai::getPriority() // This function will check if there is an enemy, if he's in range to attack or move to a random point on the map
 {
     Target status = checkEnemy();
+    std::pair<int, int> target_coordinates;
 
     if (status == IN_RANGE && this->targetStatus != IN_RANGE)
         attackEnemy();
@@ -21,18 +61,20 @@ void Ai::getPriority() // This function will check if there is an enemy, if he's
         moveToEnemy();
     else if (status == ABSENT && this->targetStatus != ABSENT)
         setRandomTarget();
+    else if (status == ABSENT && this->targetStatus == ABSENT)
+        moveToNextTile();
 }
 
 void Ai::moveToEnemy() // This function will move set the target position of the ai to the current position of the enemy
 {
-    Vector2 enemyPos = getEnemyPosition();
+    RAYLIB::Vector2 enemyPos = getEnemyPosition();
 
     this->targetPosition = enemyPos;
 }
 
-Vector2 Ai::getEnemyPosition() // This function will get the position of the enemy within range
+RAYLIB::Vector2 Ai::getEnemyPosition() // This function will get the position of the enemy within range
 {
-    Vector2 pos = {0, 0};
+    RAYLIB::Vector2 pos = {0, 0};
 
     return pos;
 }
@@ -42,9 +84,16 @@ void Ai::attackEnemy() // This function will handle the attack system of the ai
 
 }
 
-void Ai::setRandomTarget() // This function will set the target position to a random position on the map
+void Ai::setRandomTarget() // This function will set the target position to a random position on the map using a* algorithm
 {
+    int size_x = this->map[0].size();
+    int size_y = this->map.size();
 
+    srand(time(NULL));
+    while (map[this->targetPosition.x][this->targetPosition.y] != ' ' || map[this->targetPosition.x][this->targetPosition.y] != 'S') {
+        this->targetPosition.x = rand() % size_x;
+        this->targetPosition.y = rand() % size_y;
+    }
 }
 
 std::string Ai::serialize()
