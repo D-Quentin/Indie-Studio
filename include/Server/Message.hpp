a8d98e12fafe07fec8c0b7b34e56dd1f197ac2cc
Aurelien BENARD <aurelien.benard@epitech.eu>
2021-06-15 17:02:18 +0200

    [Fix] compilation error

--- include/Server/Message.hpp
+++ include/Server/Message.hpp
@@ -12,11 +12,11 @@
 
 static const int MSG_MAX_LEN = 1024;
 
-static const std::string TIMEOUT_CONNECTION = "##408##";
-static const std::string ACCEPTED_CONNECTION = "##201##";
-static const std::string INCOMMING_CONNECTION = "##100##";
-static const std::string CLOSING_SERVER = "##500##";
-static const std::string CLOSING_CONNECTION = "##501##";
-static const std::string CLIENT_CLOSING_CONNECTION = "#502#"
+static const std::string INCOMMING_CONNECTION = "##100##"; // New connection from a client
+static const std::string ACCEPTED_CONNECTION = "##201##"; // New connection is accepted
+static const std::string TIMEOUT_CONNECTION = "##408##"; // Connection timeout
+static const std::string CLOSING_SERVER = "##500##"; // Server is closing (host disconnected)
+static const std::string CLOSING_CONNECTION = "##501##"; // Server is closing this code is sent to all client
+static const std::string CLIENT_CLOSING_CONNECTION = "#502#"; // A client (non-host) disconnected from the server (Escape or CTRL-C)
 
 #endif /* !MESSAGE_HPP_ */
