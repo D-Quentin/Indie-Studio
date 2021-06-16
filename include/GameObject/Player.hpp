91537bb659412f2a4d3ef81418de4fea47f5f9a2
Mehdi Mourjane <mehdi.mourjane@epitech.eu>
2021-06-16 22:34:10 +0200

    [ADD] missed file from prev commit

--- include/GameObject/Player.hpp
+++ include/GameObject/Player.hpp
@@ -20,25 +20,19 @@ class Player : public EntityObjects {
         void gest(Client *&client);
         std::string serialize();
         void deserialize(std::string);
-<<<<<<< HEAD
         RAYLIB::Vector2 getPos() {return _pos;};
         void setShield() {_shield = true;};
         void dash();
         float _rota;
         rl::Models _model;
-=======
->>>>>>> dd5ae18233f3d5c0067eadf6c9a2f34fe5a7f448
 
     protected:
     private:
         bool _me;
-<<<<<<< HEAD
         bool _shield = false;
         RAYLIB::Vector2 _pos;
         float _ypos = 0;
         float _scale;
-=======
->>>>>>> dd5ae18233f3d5c0067eadf6c9a2f34fe5a7f448
 };
 
 #endif /* !PLAYER_HPP_ */
