dbb24e25146a7194809538e400703611dd4bbaf1
Mehdi Mourjane <mehdi.mourjane@epitech.eu>
2021-06-18 01:42:09 +0200

    [FIX] upsideDown player model

--- src/GameObject/EntityObjects.cpp
+++ src/GameObject/EntityObjects.cpp
@@ -15,7 +15,7 @@ int EntityObjects::getHealth()
 void EntityObjects::draw()
 {
     RAYLIB::Vector3 vScale = {_scale , _scale, _scale};
-    RAYLIB::Vector3 rotationAxis = { 0.0f, 1.0f, 0.0f };
+    RAYLIB::Vector3 rotationAxis = { 0.0f, 1, 0.0f };
     auto pos = this->getPos();
 
     RAYLIB::DrawModelEx(_model._model, {pos.x, 0, pos.y}, rotationAxis, _rota, vScale, RAYLIB::GRAY);
