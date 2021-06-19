8b925f431b21b87148fd43353f6668d3722eb567
Alexis THOMAS <alexis.thomas@epitech.eu>
2021-06-19 22:17:26 +0200

    [ADD] Sound class

--- include/Encapsulation/Sound.hpp
+++ include/Encapsulation/Sound.hpp
@@ -8,7 +8,31 @@
 #ifndef SOUND_HPP_
 #define SOUND_HPP_
 
+#include "include.hpp"
 
-
+class Sound
+{
+    public:
+        Sound();
+        ~Sound();
+        void init();
+        void playSniperShoot();
+        void playGunShoot();
+        void playRiffleShoot();
+        void playWallBreak();
+        void playTakeDamage();
+        void playMenuMusic();
+        void playGameMusic();
+        void playEndMusic();
+    private:
+        RAYLIB::Music _sniperShoot;
+        RAYLIB::Music _gunShoot;
+        RAYLIB::Music _riffleShoot;
+        RAYLIB::Music _wallBreak;
+        RAYLIB::Music _takeDamage;
+        RAYLIB::Music _menuMusic;
+        RAYLIB::Music _gameMusic;
+        RAYLIB::Music _endMusic;
+};
 
 #endif /* !SOUND_HPP_ */
